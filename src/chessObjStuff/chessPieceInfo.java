ad41f5b789451b11e1b30499b87fad53379ad0b4
Riz <mrk_rizwan@hotmail.com>
2018-10-24 00:12:41 -0400

    Add files via upload

---
+++ src/chessObjStuff/chessPieceInfo.java
@@ -0,0 +1,41 @@
+package chessObjStuff;
+
+public class chessPieceInfo {
+
+String piece;
+int id;
+String color;
+int firstMove;
+
+public chessPieceInfo()
+{
+}
+
+
+public chessPieceInfo(String piece, String color,int id,int firstMove)
+{
+this.piece = piece;
+this.color = color;
+this.id = id;
+this.firstMove = firstMove;
+}
+	
+//THIS RETURNS THE ID OF THE PIECE
+
+public void setID(int id)
+{
+this.id = id;
+}
+
+
+public int getID()
+{
+return this.id;
+}
+
+public String getName()
+{
+return this.piece;
+}
+	
+}
