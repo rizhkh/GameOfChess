f745a0a0f2d651163d27fe4496edf767db7c59a5
Riz <mrk_rizwan@hotmail.com>
2018-10-24 15:04:47 -0400

    Update chessPieceInfo.java

--- src/chessObjStuff/chessPieceInfo.java
+++ src/chessObjStuff/chessPieceInfo.java
@@ -14,28 +14,38 @@ public chessPieceInfo()
 
 public chessPieceInfo(String piece, String color,int id,int firstMove)
 {
-this.piece = piece;
-this.color = color;
-this.id = id;
-this.firstMove = firstMove;
+	this.piece = piece;
+	this.color = color;
+	this.id = id;
+	this.firstMove = firstMove;
 }
 	
 //THIS RETURNS THE ID OF THE PIECE
 
 public void setID(int id)
 {
-this.id = id;
+	this.id = id;
 }
 
 
+public int setFirstMove(int firstMove)
+{
+	return this.firstMove = firstMove;	
+}
+
+public int getFirstMove()
+{
+	return this.firstMove;	
+}
+
 public int getID()
 {
-return this.id;
+	return this.id;
 }
 
 public String getName()
 {
-return this.piece;
+	return this.piece;
 }
 	
 }
